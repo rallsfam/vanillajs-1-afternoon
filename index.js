8fb4683305e8015f94a1ecc217f275fb0c2aa697
Jason Ralls <admin@MacBook-Pro.local>
2020-06-16 17:59:35 -0400

    Changes made

---
+++ index.js
@@ -0,0 +1,13 @@
+const idInput = document.getElementById('idInput');
+const colorInput = document.getElementById('colorInput');
+
+function setCard() {
+    const card = document.getElementById(idInput.value);
+    card.style.color = colorInput.value;
+}
+    console.log(card)
+
+}
+
+// console.log(idInput)
+// console.log(colorInput)
